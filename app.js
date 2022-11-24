08c95765eeffcf1f88e3b85746b4e2a37369f423
@javedakeeb1994 <javedakeeb1994@mail.com>
2022-11-24 15:29:26 +0530

    final commit

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const emailArray = [];
+// const emailArray = [];
 
+// const emailValue = emailInput.value;
+// const Button = document.getElementById("inputButton");
+
+// function emailFunction() {
+//   const emailInput = document.getElementById("email");
+//   const emailValue = emailInput.value;
+//   Button.innerHTML = "Thank you";
+//   emailArray.push(emailValue);
+//   console.log(emailArray);
+
+//   return false;
+// }
+
+// Button.addEventListener("click", emailFunction);
+
+const emailInput = document.getElementById("email");
 const Button = document.getElementById("inputButton");
+const response = document.getElementById("response");
 
-function emailFunction() {
-  const emailInput = document.getElementById("email");
-  const emailValue = emailInput.value;
-  emailArray.push(emailValue);
-  console.log(emailArray);
-  return false;
+Button.addEventListener("click", function () {
+  const email = emailInput.value;
+  if (validateEmail(email)) {
+    response.innerHTML = "Hiya Cowboy, this email will work for us 🤠";
+    Button.innerHTML = "Thank you";
+  } else {
+    response.innerHTML = "Sorry, this email is not cool enough 😩";
+  }
+});
+
+function validateEmail(email) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
-
-Button.addEventListener("click", emailFunction);
